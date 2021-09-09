48f400054d28648e34b5b0dc19f89e16d894af63
AI Bulbull <85570436+aibulbull@users.noreply.github.com>
2021-09-09 10:31:19 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//text change 1
+function textChange(){
+  document.getElementById('lorem').innerHTML="how was it????";
+  document.getElementById('lorem').style.color="red";
+}
+//text change 2
+function textChange1(){
+  document.getElementById('lorem1').innerHTML="how was it????";
+}
+
+//text change 2
+function textChange2(){
+  document.getElementsByClassName('classtest')[2].innerHTML="Thank you";
+}
+
+//text change 3
+function textChange3(){
+  document.getElementById('lorem3').innerHTML="how was it????";
+}
+//text change 4
+function textChange4(){
+  document.getElementById('lorem4').innerHTML="how was it????";
+}
+
+//text change 5
+function textChange5(){
+  document.getElementById('lorem5').innerHTML= Date();
+}
+
+//text change 5
+function alartMessage() {
+  alert("Something is wrong!!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
